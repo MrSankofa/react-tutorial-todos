bf90ab234ab4a177d04da037fe5bd45b3808b1a4
Brett Cunningham <bwcunninghamii@gmail.com>
2019-03-28 12:53:11 -0400

    saving working version of react and webpack

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var db = require('./database/pricesDB');
+
 var app = express();
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname));
+
+app.get('/price', function(req, res) {
+    db.getPriceFromDB(null, function(result) {
+        console.log('this is the result from the server', result);
+        res.json(result);
+    });
+});
+
+app.get('/price/:priceId', function(req, res) {
+    db.byIdgetPriceFromDB(null, req.params.priceId, function(result){ 
+        console.log('this is the result from the server', result);
+        res.json(result);
+    });
+});
+
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Server listening');
